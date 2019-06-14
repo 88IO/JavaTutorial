f3d8681c78fc01a9bb58f2b94ace7f75d63d834d
88IO <8810aoba@gmail.com>
2019-06-14 10:09:15 +0900

    add comments

--- Chapter2/Function.java
+++ Chapter2/Function.java
@@ -1,34 +1,43 @@
+// 外部機能のインポート
 import java.util.Scanner;
 
+// ２次方程式 f(x) = ax^2 + bx + c
 class QuadraticEqu {
     final long a, b, c;
 
+    // 係数の格納
     public QuadraticEqu(long a, long b, long c) {
         this.a = a;
         this.b = b;
         this.c = c;
     }
 
+    // 計算結果を返却
     public long calc(long n) {
         return (long)(a*Math.pow(n, 2) + b*n + c);
     }
 }
 
+// ２次方程式の導関数 f'(x) = 2ax + b
 class Derivada {
     final long a, b;
 
+    // 係数の格納(引数に２次方程式のクラスを投げる)
     public Derivada(QuadraticEqu f) {
         this.a = f.a*2;
         this.b = f.b;
     }
 
+    // 計算結果を返却
     public long calc(long n) {
         return a*n + b;
     }
 }
 
+// メインクラス
 class Function {
     public static void main(String[] args) {
+        //　入力値の格納
         Scanner stdIn = new Scanner(System.in);
 
         System.out.println("関 数 f(x) = ax ^2 + bx + c を 生 成 し ま す 。");
@@ -43,9 +52,12 @@ class Function {
         System.out.print("n: ");
         long n = stdIn.nextLong();
 
+        // ２次方程式を定義
         QuadraticEqu f = new QuadraticEqu(a, b, c);
 
+        // f(n)を計算
         System.out.println(String.format("f(%d) = %d", n, f.calc(n)));
+        // f'(n)を計算
         System.out.println(String.format("f'(%d) = %d", n, new Derivada(f).calc(n)));
     }
 }
--- Chapter2/TimeCalculater.java
+++ Chapter2/TimeCalculater.java
@@ -1,18 +1,22 @@
+// 外部機能のインポート
 import java.util.Scanner;
 
 class TimeCalculater {
     public static void main(String[] args) {
+        // 入力値を格納
         Scanner stdIn = new Scanner(System.in);
         System.out.print("時間を秒で入力してください：");
-        long clock = stdIn.nextInt();
+        long clock = stdIn.nextLong();
 
+        // 時間・分・秒を格納
         int hours = (int)clock / 3600;
+        // 取得秒数を１時間未満に更新
         clock %= 3600;
 
         int minutes = (int)clock / 60;
         int seconds = (int)clock % 60;
 
-        System.out.println(hours+"時間"+minutes+"分"+seconds+"秒です");
+        // ２桁固定で出力
         System.out.println(String.format("%02d時間%02d分%02d秒です", hours, minutes, seconds));
     }
 }
