41200ee64c6cded6e18408133d3d8659bcae2ee6
88IO <8810aoba@gmail.com>
2019-07-03 20:30:47 +0900

    FlowerNumber : String -> String[]

--- Chapter5/PrintFlowerNumber.java
+++ Chapter5/PrintFlowerNumber.java
@@ -4,66 +4,66 @@ class PrintFlowerNumber {
     public static void main(String[] args) {
         Scanner stdIn = new Scanner(System.in);
 
-        String[] flowerNum = new String[10];
+        String[][] flowerNum = new String[10][];
         String[] strOut = {"", "", "", "", ""};
 
-        flowerNum[0] = "＊＊＊" +
-                       "＊　＊" +
-                       "＊　＊" +
-                       "＊　＊" +
-                       "＊＊＊";
-        flowerNum[1] = "　　＊" +
-                       "　　＊" +
-                       "　　＊" +
-                       "　　＊" +
-                       "　　＊";
-        flowerNum[2] = "＊＊＊" +
-                       "　　＊" +
-                       "＊＊＊" +
-                       "＊　　" +
-                       "＊＊＊";
-        flowerNum[3] = "＊＊＊" +
-                       "　　＊" +
-                       "＊＊＊" +
-                       "　　＊" +
-                       "＊＊＊";
-        flowerNum[4] = "＊　＊" +
-                       "＊　＊" +
-                       "＊＊＊" +
-                       "　　＊" +
-                       "　　＊";
-        flowerNum[5] = "＊＊＊" +
-                       "＊　　" +
-                       "＊＊＊" +
-                       "　　＊" +
-                       "＊＊＊";
-        flowerNum[6] = "＊＊＊" +
-                       "＊　　" +
-                       "＊＊＊" +
-                       "＊　＊" +
-                       "＊＊＊";
-        flowerNum[7] = "＊＊＊" +
-                       "　　＊" +
-                       "　　＊" +
-                       "　　＊" +
-                       "　　＊";
-        flowerNum[8] = "＊＊＊" +
-                       "＊　＊" +
-                       "＊＊＊" +
-                       "＊　＊" +
-                       "＊＊＊";
-        flowerNum[9] = "＊＊＊" +
-                       "＊　＊" +
-                       "＊＊＊" +
-                       "　　＊" +
-                       "＊＊＊";
+        flowerNum[0] = new String[]{"＊＊＊",
+                                    "＊　＊",
+                                    "＊　＊",
+                                    "＊　＊",
+                                    "＊＊＊"};
+        flowerNum[1] = new String[]{"　　＊",
+                                    "　　＊",
+                                    "　　＊",
+                                    "　　＊",
+                                    "　　＊"};
+        flowerNum[2] = new String[]{"＊＊＊",
+                                    "　　＊",
+                                    "＊＊＊",
+                                    "＊　　",
+                                    "＊＊＊"};
+        flowerNum[3] = new String[]{"＊＊＊",
+                                    "　　＊",
+                                    "＊＊＊",
+                                    "　　＊",
+                                    "＊＊＊"};
+        flowerNum[4] = new String[]{"＊　＊",
+                                    "＊　＊",
+                                    "＊＊＊",
+                                    "　　＊",
+                                    "　　＊"};
+        flowerNum[5] = new String[]{"＊＊＊",
+                                    "＊　　",
+                                    "＊＊＊",
+                                    "　　＊",
+                                    "＊＊＊"};
+        flowerNum[6] = new String[]{"＊＊＊",
+                                    "＊　　",
+                                    "＊＊＊",
+                                    "＊　＊",
+                                    "＊＊＊"};
+        flowerNum[7] = new String[]{"＊＊＊",
+                                    "　　＊",
+                                    "　　＊",
+                                    "　　＊",
+                                    "　　＊"};
+        flowerNum[8] = new String[]{"＊＊＊",
+                                    "＊　＊",
+                                    "＊＊＊",
+                                    "＊　＊",
+                                    "＊＊＊"};
+        flowerNum[9] = new String[]{"＊＊＊",
+                                    "＊　＊",
+                                    "＊＊＊",
+                                    "　　＊",
+                                    "＊＊＊"};
 
         System.out.print("数字を入力してください：");
         String[] strArray = stdIn.next().split("");
 
         for (String s : strArray) {
             for (int i = 0; i < 5; i++){
-                strOut[i] += (flowerNum[Integer.parseInt(s)].substring(i * 3, i * 3 + 3) + " ") ;
+                strOut[i] += (flowerNum[Integer.parseInt(s)][i] + " ") ;
             }
         }
 
